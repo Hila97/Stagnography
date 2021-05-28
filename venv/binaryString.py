0237e2af115f97495cac1cef21497c8aaf99f8d2
Hila Pinker <64475686+Hila97@users.noreply.github.com>
2021-05-28 16:02:21 +0300

    start

---
+++ venv/binaryString.py
@@ -0,0 +1,9 @@
+test_str = "this is a try"
+print("The original string is : " + str(test_str))
+res = ''.join(format(ord(i), '08b') for i in test_str)
+print("The string after binary conversion : " + str(res))
+
+def bits2a(b):
+    return ''.join(chr(int(''.join(x), 2)) for x in zip(*[iter(b)]*8))
+
+print(bits2a(res))
